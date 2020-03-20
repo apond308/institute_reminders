8354d7612bb8a9a9cdf2d8884eba9b56407076e5
Andrew Pond <apond308@gmail.com>
2020-03-20 10:29:11 -0600

    allowed emailer to be executable

--- emailer.py
+++ emailer.py
@@ -1,13 +1,21 @@
 import requests
 import json
 
-url = "https://3u42tnm3ll.execute-api.us-west-2.amazonaws.com/Email_Sender"
 
 def sendEmail(recipients, subject, message):
     payload = {"emails": [], "subject": subject, "message": message}
     for number in recipients:
         payload["emails"].append(number)
     headers = {"username": "ses_api"}
+    url = "https://3u42tnm3ll.execute-api.us-west-2.amazonaws.com/Email_Sender"
     response = requests.post(url, headers=headers, json=payload)
     return response
-    
+
+
+if __name__ == "__main__":
+    import sys
+    if (len(sys.argv) > 1):
+        recipients = [sys.argv[1]]
+    else:
+        recipients = ["apond308@gmail.com"]
+    sendEmail(recipients, "Test Email", "This is a test email.")
